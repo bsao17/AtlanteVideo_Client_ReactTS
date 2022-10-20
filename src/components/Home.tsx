3816a6b2c9a76617d98b03af0ce4b870a47ced4b
Bruno M <declic62gmail.com>
2022-10-20 09:36:36 +0200

    feat: add TitleCard.tsx with focus

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import TitleCard from "./TitleCard";
 
 
 function Home() {
     return (
-        <div className="flex flex-col justify-center items-center">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div>
+            <TitleCard title="Accueil"/>
+            <img src={logo} className="App-logo" alt="logo" />
         </div>
     );
 }
