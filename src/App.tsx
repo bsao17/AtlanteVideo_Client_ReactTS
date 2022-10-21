3c0258062bf6abd8e1c21f9b130e4692cd0e678a
Bruno M <declic62gmail.com>
2022-10-21 12:29:54 +0200

    fix: index in components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: FunctionComponent = () => {
     <div className="App bg-neutral-800">
         <Router>
             <ul className="flex flex-row justify-around p-5 text-blue-200">
-                <Link to="/" className="font-serif font-semibold text-xs sm:text-xs md:text-xs lg:text-lg xl:text-xl focus:underline">Accueil</Link>
+                <Link to="/" className="font-serif font-semibold text-xs sm:text-xs md:text-xs lg:text-lg xl:text-xl focus:underline focus:text-white">Accueil</Link>
                 <Link to="/product" className="font-serif font-semibold text-xs sm:text-xs md:text-xs lg:text-lg xl:text-xl focus:text-white focus:underline">Services & produits</Link>
                 <Link to="/about" className="font-serif font-semibold text-xs sm:text-xs md:text-xs lg:text-lg xl:text-xl focus:text-white focus:underline">A propos</Link>
             </ul>
