19e2a1d3dca080cbb41f238d454d15e0fb2124dd
Bruno M <declic62gmail.com>
2022-10-21 09:53:54 +0200

    feat: add tests for wordings in navigation bar

--- src/__TEST__/AppLink.test.js
+++ src/__TEST__/AppLink.test.js
@@ -3,10 +3,16 @@ import {render, screen} from "@testing-library/react";
 
 describe("vérifier la présence des lien et leurs fonctionnement", () => {
     beforeEach(()=>{
+        // eslint-disable-next-line testing-library/no-render-in-setup
         render(<App/>)
     })
-    it("test de la présence du lien accueil", () => {
-        const home = screen.getByText("Accueil");
-        expect(home).toBeInTheDocument();
+    test("de la présence du lien accueil", () => {
+        expect(screen.getByText("Accueil")).toBeInTheDocument();
+    })
+    test("de la présence du lien service", () => {
+        expect(screen.getByText("Services & produits")).toBeInTheDocument();
+    })
+    test("de la présence du lien a propos", () => {
+        expect(screen.getByText("A propos")).toBeInTheDocument();
     })
 })
