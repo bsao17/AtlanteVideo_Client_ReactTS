445e5d4bb6197768244b4ba90248d5502d2bc2fa
Bruno M <declic62gmail.com>
2022-10-23 13:46:47 +0200

    wip: wrap navigation in Navigation.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import TitleCard from "./TitleCard";
 import home_presentation_anafi from "../assets/drone_anafi.png";
 import home_presentation_dji from "../assets/drone_dji.png";
+import {Context} from "../context/Context"
 
 function Home() {
+    // @ts-ignore
+    const myContext = useContext(Context)
     return (
         <>
             <TitleCard title="Atlante-Video-Production"/>
@@ -11,12 +14,14 @@ function Home() {
             columns-2
             flex-col
             justify-end
-            pt-9 md:block
+            pt-9
+            md:block
             rounded
             mx-24
             ">
-                <div className="col-span-1 mb-28 flex justify-center hover:shadow hover:rounded-full">
-                    <img src={home_presentation_anafi} className="hidden md:block sm:block xl:block pt-4 hover:opacity-50  z-10" alt="logo" />
+                <div className="mb-28 flex flex-col justify-center items-center hover:shadow">
+                    <img src={home_presentation_anafi} className="hidden md:block sm:block xl:block pt-4 hover:opacity-50  z-10" alt="logo"/>
+                    <div className="hover:font-semibold">{myContext.droneName}</div>
                 </div>
                 <div className="col-span-2 flex justify-center rounded-full">
                     <img src={home_presentation_dji} className="hidden md:block sm:block xl:block pt-4 hover:opacity-50 z-10" alt="logo" />
