e075fda8079a5255d0b46002da4883ad7a612dcf
Bruno M <declic62gmail.com>
2022-11-01 12:05:51 +0100

    feat: responsive menu ok

--- src/components/NavigationResponsive.tsx
+++ src/components/NavigationResponsive.tsx
@@ -1,7 +1,15 @@
+import {MouseEventHandler} from "react";
 
-const NavigationResponsive = () => {
+type props = {
+    handleNavigationBar: MouseEventHandler
+}
+
+const NavigationResponsive = ({handleNavigationBar}: props) => {
+
     return (
-        <div className=" md:hidden sm:hidden lg:hidden p-3 cursor-pointer">
+        <div className=" md:hidden sm:hidden lg:hidden p-3 cursor-pointer"
+            onClick={handleNavigationBar}
+        >
             <div className="w-9 h-1 bg-white mb-1 rounded-full"/>
             <div className="w-9 h-1 bg-white mb-1 rounded-full"/>
             <div className="w-9 h-1 bg-white mb-1 rounded-full"/>
