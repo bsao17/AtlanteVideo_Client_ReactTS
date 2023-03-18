4f032d90d772970fc83d2c55ba54aed9f8a8ebeb
Bruno M <declic62gmail.com>
2023-03-18 21:13:45 +0100

    feat: add text and animation/images

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,10 @@
-import React, {useContext} from "react";
+import React from "react";
 import {TitleCard, SetProfile} from "../components";
 import {FunctionComponent} from "react";
 import {Link} from "react-router-dom";
-import {Context} from "../context/Context";
+
 
 const About: FunctionComponent = ()=>{
-    const context = useContext(Context)
-
     return(
         <div>
             <TitleCard title={"Historique et projets d'Atlante-Production"}/>
@@ -14,13 +12,13 @@ const About: FunctionComponent = ()=>{
                 <SetProfile/>
             </div>
              <div className="text-black bg-gray-300 mx-10 text-justify font-serif border border-2 border-white shadow shadow-5-strong rounded text-lg p-5">
-                Bienvenue chez Atlante Création Numérique (ACN),<br/><br/>
+                Bienvenue chez <span className="font-bold text-2xl text-blue-900 bg-white rounded shadow p-2 italic">Atlante Création Numérique</span> (ACN),<br/><br/>
                  Là où l&lsquo;imagination prend vie et où vos idées se
                  transforment en réalités captivantes. <br/><br/>
                  Nous sommes une entreprise de création multimédia et de développement web et informatique qui a pour vocation de fournir
                  des solutions innovantes, personnalisées et personnalisables afin de donner vie à vos projets. <br/><br/>
 
-                <span className="font-light text-xl bg-white rounded p-2 italic shadow shadow-5">Vous cherchez à captiver votre audience et à laisser une impression durable ?</span><br/><br/>
+                <span className="font-bold text-xl bg-white text-blue-800 rounded p-2 italic shadow ">Vous cherchez à captiver votre audience et à laisser une impression durable ?</span><br/><br/>
 
                  ACN est là pour vous. <br/><br/>
                  Notre équipe d&lsquo;experts passionnés maîtrise un large éventail de compétences, notamment la réalisation
@@ -44,7 +42,7 @@ const About: FunctionComponent = ()=>{
                 N&lsquo;attendez plus, <Link to="/Home"> contactez-nous</Link> et découvrez comment Atlante Création Numérique peut donner vie
                  à vos projets, développer votre présence en ligne et vous aider à marquer les esprits. <br/><br/>
 
-                 Bienvenue dans le futur de la création numérique !
+                 <span className="font-bold text-xl text-blue-800">Bienvenue dans le futur de la création numérique !</span>
             </div>
         </div>
     );
