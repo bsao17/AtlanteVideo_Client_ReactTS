fa6689176b49efc77c7ec58ec95b7df12ffa2cfa
Bruno M <declic62gmail.com>
2023-03-22 11:07:54 +0100

    feat: refacto, design update for VideoGrid.tsx and About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,52 +1,62 @@
 import React from "react";
-import {TitleCard, SetProfile} from "../components";
-import {FunctionComponent} from "react";
-import {Link} from "react-router-dom";
-import styles from "./style/about.module.css"
+import { TitleCard, SetProfile } from "../components";
+import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import styles from "./style/about.module.css";
 
-
-const About: FunctionComponent = ()=>{
-    return(
-        <div>
-            <TitleCard title={"Historique et projets d'Atlante-Production"}/>
-            <div>
-                <SetProfile/>
+const About: FunctionComponent = () => {
+    return (
+        <div className=" min-h-screen">
+            <TitleCard title={"Historique et projets d'Atlante-Production"} />
+            <div className="flex justify-center">
+                <SetProfile />
             </div>
-             <div className="text-black bg-gray-300 mx-10 text-justify font-serif border-2 border-white shadow shadow-5-strong rounded text-lg p-5">
-                Bienvenue chez <span id={styles.title} className="font-bold text-2xl text-blue-900 bg-white rounded shadow p-2 italic">Atlante Création Numérique</span> (ACN),<br/><br/>
-                 Là où l&lsquo;imagination prend vie et où vos idées se
-                 transforment en réalités captivantes. <br/><br/>
-                 Nous sommes une entreprise de création multimédia et de développement web qui a pour vocation de fournir
-                 des solutions innovantes, personnalisées et personnalisables afin de donner vie à vos projets. <br/><br/>
+            <div className="mx-10 my-10 p-6 md:p-10 bg-white rounded-lg shadow-lg text-justify font-serif text-lg leading-relaxed">
+                <h2 className={`${styles.title} font-bold text-3xl text-blue-900 mb-6 italic`}>
+                    Atlante Création Numérique
+                </h2>
+                <p>
+                    Bienvenue chez <span className="font-bold">Atlante Création Numérique</span> (ACN), là où l&lsquo;imagination prend
+                    vie et où vos idées se transforment en réalités captivantes.
+                </p>
+                <p className="mt-4">
+                    Nous sommes une entreprise de création multimédia et de développement web qui a pour vocation de fournir des
+                    solutions innovantes, personnalisées et personnalisables afin de donner vie à vos projets.
+                </p>
 
-                <span className="font-bold text-xl bg-white text-blue-800 rounded p-2 italic shadow ">Vous cherchez à captiver votre audience et à laisser une impression durable ?</span><br/><br/>
+                <h3 className="font-bold text-xl text-blue-800 mt-6 mb-4 italic">Vous cherchez à captiver votre audience et à laisser une impression durable ?</h3>
 
-                 ACN est là pour vous. <br/><br/>
-                 Notre équipe d&lsquo;experts passionnés maîtrise un large éventail de compétences, notamment la réalisation
-                 de vidéos, le montage vidéo, le marketing digital, le sound design, le Motion Design, la prise de vidéos
-                 et photos aériennes à l&lsquo;aide de drones professionnels, le développement informatique et
-                 la création de sites et d&lsquo;applications web. <br/><br/>
-                 Nous sommes également spécialisés dans la vérification de toitures, la réalisation de vidéos publicitaires
-                 et la création de visites virtuelles pour les commerces. <br/><br/>
-
-                Chez ACN, nous croyons en l&lsquo;importance de comprendre vos besoins spécifiques et de travailler en étroite
-                 collaboration avec vous pour créer des contenus percutants et de haute qualité qui reflètent votre vision. <br/><br/>
-
-                 Que vous soyez une start-up cherchant à se démarquer, une entreprise établie souhaitant renouveler son image
-                 ou à la recherche d&lsquo;une solution web ou informatique sur mesure, nous sommes prêts à relever le défi
-                 et à vous aider à atteindre de nouveaux sommets. <br/><br/>
-
-                Laissez-nous transformer votre vision en une expérience inoubliable pour votre public. <br/>
-                 Faites confiance à ACN pour vous propulser dans l&lsquo;univers du numérique et rejoignez dès aujourd&lsquo;hui
-                 notre liste de clients satisfaits. <br/><br/>
-
-                N&lsquo;attendez plus, <Link to="/Home"> contactez-nous</Link> et découvrez comment Atlante Création Numérique peut donner vie
-                 à vos projets, développer votre présence en ligne et vous aider à marquer les esprits. <br/><br/>
-
-                 <span className="font-bold text-xl text-blue-800">Bienvenue dans le futur de la création numérique !</span>
+                <p>
+                    ACN est là pour vous. Notre équipe d&lsquo;experts passionnés maîtrise un large éventail de compétences, notamment la
+                    réalisation de vidéos, le montage vidéo, le marketing digital, le sound design, le Motion Design, la prise de
+                    vidéos et photos aériennes à l&lsquo;aide de drones professionnels, le développement informatique et la création de
+                    sites et d&lsquo;applications web. Nous sommes également spécialisés dans la vérification de toitures, la réalisation
+                    de vidéos publicitaires et la création de visites virtuelles pour les commerces.
+                </p>
+                <p className="mt-4">
+                    Chez ACN, nous croyons en l&lsquo;importance de comprendre vos besoins spécifiques et de travailler en étroite
+                    collaboration avec vous pour créer des contenus percutants et de haute qualité qui reflètent votre vision.
+                </p>
+                <p className="mt-4">
+                    Que vous soyez une start-up cherchant à se démarquer, une entreprise établie souhaitant renouveler son image
+                    ou à la recherche d&lsquo;une solution web ou informatique sur mesure, nous sommes prêts à relever le défi et à vous
+                    aider à atteindre de nouveaux sommets.
+                </p>
+                <p className="mt-4">
+                    Laissez-nous transformer votre vision en une expérience inoubliable pour votre public. Faites confiance à ACN
+                    pour vous propulser dans l&lsquo;univers du numérique et rejoignez dès aujourd&lsquo;hui notre liste de clients satisfaits.
+                </p>
+                <p className="mt-4">
+                    N&lsquo;attendez plus, <Link to="/Home" className="text-blue-600 underline">contactez-nous</Link> et découvrez comment Atlante Création Numérique peut donner vie
+                    à vos projets, développer votre présence en ligne et vous aider à marquer les esprits.
+                </p>
+                <p className="mt-4 font-bold text-xl text-blue-800">
+                    Bienvenue dans le futur de la création numérique !
+                </p>
             </div>
         </div>
-    );
+);
 };
 
 export default About;
+
--- src/pages/VideoGrid.tsx
+++ src/pages/VideoGrid.tsx
@@ -1,24 +1,79 @@
-import React from "react";
-import {TitleCard} from "../components";
-import ReactPlayer from 'react-player';
+import React, { useState } from "react";
+import { TitleCard } from "../components";
+import ReactPlayer from "react-player";
 
-const VideoGrid = () => {
+interface VideoItem {
+    url: string;
+    thumbnail: string;
+}
+
+const VideoGrid: React.FC = () => {
+    const [selectedVideo, setSelectedVideo] = useState<string | null>(null);
+
+    const videos: VideoItem[] = [
+        {
+            url: "https://www.youtube.com/watch?v=1sF1CZVQ8ts",
+            thumbnail: "https://img.youtube.com/vi/1sF1CZVQ8ts/maxresdefault.jpg",
+        },
+        {
+            url: "https://youtu.be/mjolL7sx_Ps",
+            thumbnail: "https://img.youtube.com/vi/mjolL7sx_Ps/maxresdefault.jpg",
+        },
+        {
+            url: "https://youtu.be/CqfZ8_0wlRI",
+            thumbnail: "https://img.youtube.com/vi/CqfZ8_0wlRI/maxresdefault.jpg",
+        },
+        {
+            url: "https://youtu.be/vdE7zrsUHQI",
+            thumbnail: "https://img.youtube.com/vi/vdE7zrsUHQI/maxresdefault.jpg",
+        },
+        {
+            url: "https://youtu.be/iEehqSCLyYE",
+            thumbnail: "https://img.youtube.com/vi/iEehqSCLyYE/maxresdefault.jpg",
+        },
+        {
+            url: "https://youtu.be/vdE7zrsUHQI",
+            thumbnail: "https://img.youtube.com/vi/vdE7zrsUHQI/maxresdefault.jpg",
+        },
+        // Ajoutez vos vidéos ici
+    ];
+
     return (
         <div>
-            <TitleCard title="Portfolio Video"/>
-                <div className="flex justify-center flex-wrap">
-                    <div className="grid grid-cols-auto ">
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/iEehqSCLyYE"}/>
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/FsAYlRVQHnA"}/>
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/m4qIzeMSpPM"}/>
-                    </div>
-                    <div className="grid grid-flow-dense">
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/-FD5AvrKx9I"}/>
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/9ZKebXCb-24"}/>
-                        <ReactPlayer className="m-5 hover:border-3 hover:border-4 hover:border-white rounded-lg" url={"https://youtu.be/tQ2DoGhcl1s"}/>
-                    </div>
+            <TitleCard title="Portfolio Video" />
+            <div className="flex justify-center flex-wrap p-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {videos.map((video, index) => (
+                        <div
+                            key={index}
+                            className="relative overflow-hidden rounded-lg group cursor-pointer"
+                            onClick={() => setSelectedVideo(video.url)}
+                        >
+                            <img
+                                src={video.thumbnail}
+                                alt="Video thumbnail"
+                                className="w-full transition-transform duration-300 ease-in-out transform group-hover:scale-105"
+                            />
+                            <div className="absolute inset-0 transition-opacity duration-300 ease-in-out bg-gradient-to-tr from-black via-transparent to-black opacity-0 group-hover:opacity-50" />
+                        </div>
+                    ))}
                 </div>
             </div>
+            {selectedVideo && (
+                <div
+                    className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75"
+                    onClick={() => setSelectedVideo(null)}
+                >
+                    <ReactPlayer
+                        className="rounded-lg"
+                        width="80%"
+                        height="80%"
+                        playing={true}
+                        url={selectedVideo}
+                    />
+                </div>
+            )}
+        </div>
     );
 };
 
