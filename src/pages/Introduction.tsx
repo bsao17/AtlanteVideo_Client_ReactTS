2e692bee35cf4b5e15993cddeb656e377bd91b26
Bruno M <declic62gmail.com>
2023-04-03 10:13:00 +0200

    feat: update product page

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {IntroAnimation} from "../components/IntroAnimation/IntroAnimation";
 import styles from "./style/introduction.module.scss"
+import {Telephone} from "../components/telephone";
 
 const Introduction: React.FC = () => {
     return (
             <div id={styles.animation}>
+                <Telephone/>
                 <IntroAnimation/>
             </div>
         )
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import {TitleCard} from "../components";
-import {FunctionComponent} from "react";
+import { TitleCard } from "../components";
+import { FunctionComponent } from "react";
 
-
-const Product: FunctionComponent = ()=>{
+const Product: FunctionComponent = () => {
     return (
         <div>
-            <TitleCard title="Nos prestations"/>
-            <div className="flex items-stretch  items-center gap-2 m-5 ">
-                <div className="h-full w-1/3 bg-gray-600 border border-white rounded-xl ">
-                    <h3 className="sm:text-sm lg:text-xl text-center text-white mb-3">Réalisation de vidéos</h3>
-                    <div className="bg-white rounded-xl text-black text-justify p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda delectus dolorum exercitationem illum necessitatibus odit porro, possimus tempora vel voluptatibus. Ab aperiam autem blanditiis, consequuntur cupiditate dignissimos dolore eligendi eos esse eum id iure, labore laborum laudantium, magni maxime natus necessitatibus nostrum nulla quasi repellat saepe sint sit soluta voluptatibus?</div>
-                </div>
-                <div className="h-full w-1/3 bg-gray-600 border hover:border-white rounded-xl ">
-                    <h3 className="sm:text-sm lg:text-xl text-center text-white mb-3">réalisation de site web <br/> et <br/> app-mobile</h3>
-                    <div className="text-black bg-white rounded-xl text-white text-justify p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut beatae, consectetur dolor facere libero perferendis quae quis recusandae repudiandae tempore voluptatibus. Aspernatur distinctio in iste magni nobis obcaecati tenetur ut? Ad asperiores debitis dicta dolorem ducimus eaque harum laboriosam libero molestias perferendis, perspiciatis possimus quae quis quisquam repellat, sint?</div>
-                </div>
-                <div className="h-full w-1/3 bg-gray-600 border border-white rounded-xl ">
-                    <h3 className="sm:text-sm lg:text-xl text-center text-white mb-3">Motion Design</h3>
-                    <div className=" text-black bg-white rounded-xl text-white text-justify p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut delectus dignissimos dolores, esse eum facere ipsum molestiae, nam officiis quia quibusdam quis quos repellendus sapiente similique totam veritatis? Architecto deleniti distinctio doloribus dolorum est explicabo impedit sint sunt tempora ullam. Blanditiis quae, rem? Ad commodi corporis obcaecati perspiciatis similique soluta?</div>
-                </div>
+            <TitleCard title="Nos prestations" />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-5 my-10">
+                {services.map((service, index) => (
+                    <div
+                        key={index}
+                        className="bg-white shadow-lg rounded-lg p-6 border-2 border-transparent hover:border-primary transition duration-300 ease-in"
+                    >
+                        <h3 className="text-xl font-semibold text-center text-primary mb-4">
+                            {service.title}
+                        </h3>
+                        <p className="text-justify text-gray-700">{service.description}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
 };
+
+const services = [
+    {
+        title: "Réalisation de vidéos",
+        description:
+        "lorem 10"
+    },
+    {
+        title: "Réalisation de site web et app-mobile",
+        description:
+        "lorem 10"
+    },
+    {
+        title: "Motion Design",
+        description:
+        "lorem 10"
+    },
+];
 
 export default Product;
