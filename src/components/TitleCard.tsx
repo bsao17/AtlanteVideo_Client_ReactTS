318a5b6c93344808e39a3fabe2549531510aadcc
Bruno M <declic62gmail.com>
2023-04-13 14:42:14 +0200

    feat: add TitleCard.tsx gif, and update color home card

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import boule from "../assets/gif/ThreeRoundVideo.gif"
+
 const TitleCard = ({title}: {title: string}) => {
     return (
         <div>
             <h1 className="
             w-full
             text-3xl
-            text-center
+            flex
+            justify-center
+            items-center
             text-white
             p-4
             border-b-4
@@ -13,6 +17,7 @@ const TitleCard = ({title}: {title: string}) => {
             bg-neutral-800
             font-serif"
             >
+                <img className="absolute right-0 rounded-full mx-10 border border-gray-500" src={boule} alt="logo boule" width={90}/>
                 {title}
             </h1>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,12 @@ module.exports = {
         extend: {
             boxShadow: {
                 '6-strongB': '0px 25px 50px -12px rgba(0, 0, 20, 0.4)', // Ajustez cette valeur en fonction de l'intensité souhaitée
-                '6-strongT': '0px -25px 50px -12px rgba(0, 0, 20, 0.4)', // Ajustez cette valeur en fonction de l'intensité souhaitée
+                '6-strongLogo': '-15px 10px 10px -10px rgba(80, 80, 80, 0.5)', // Ajustez cette valeur en fonction de l'intensité souhaitée
 
             },
         },
     },
     plugins: [
-        // eslint-disable-next-line no-undef
-        require("tw-elements/dist/plugin")
+        // eslint-disable-next-line no-unde10require("tw-elements/dist/plugin")
     ],
 };
