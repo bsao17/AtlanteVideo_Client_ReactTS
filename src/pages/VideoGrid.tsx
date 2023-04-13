04cfcd4163f3edff87cf1897a935255afd9a1e63
Bruno M <declic62gmail.com>
2023-04-13 20:41:21 +0200

    fix: props titleCard VideoGrid.tsx and MotionDesign.tsx

--- src/pages/VideoGrid.tsx
+++ src/pages/VideoGrid.tsx
@@ -42,7 +42,7 @@ const VideoGrid: React.FC = () => {
 
     return (
         <div>
-            <TitleCard title="Portfolio Video" />
+            <TitleCard title="Vidéos" />
             <div className="flex justify-center flex-wrap p-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {videos.map((video, index) => (
